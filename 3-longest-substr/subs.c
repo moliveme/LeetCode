e35e38c62d8b1671d1d5cbea238ddca8cc3403a6
Mridu <mprasha@purdue.edu>
2023-05-23 17:55:43 +0530

    longest substr O(n)

--- 3-longest-substr/subs.c
+++ 3-longest-substr/subs.c
@@ -79,7 +79,7 @@ int lengthOfLongestSubstring1(char * s){
 // trying O(2n)
 // yay i can successfully work sets in c
 
-int lengthOfLongestSubstring(char * s) {
+int lengthOfLongestSubstring2(char * s) {
         
     int n = strlen(s);
 
@@ -94,8 +94,6 @@ int lengthOfLongestSubstring(char * s) {
     for (int i = 0; i < 95; i++) {
         set_of_all_chars[i] = '\0';
     }
-
-    char temp = s[l];
 
     for (int r = 0; r < n; r++) {
 
@@ -124,8 +122,75 @@ int lengthOfLongestSubstring(char * s) {
 
 }
 
+// trying O(n)
+
+typedef struct el {
+    int index;
+    char val;
+} set_el;
+
+int lengthOfLongestSubstring(char * s) {
+        
+    int n = strlen(s);
+
+    printf("n %d\n", n);
+
+    int max = 0;
+    int cur_l = 0;
+    int l = 0;
+    int r = 0;
+    set_el set_of_all_chars[95];
+
+    for (int i = 0; i < 95; i++) {
+        set_of_all_chars[i].val = '\0';
+        set_of_all_chars[i].index = -1;
+    }
+
+    int old_l = l;
+
+    for (int r = 0; r < n; r++) {
+
+        printf("l %d r %d cur_l %d max %d\n", l, r, cur_l, max);
+
+        printf(" set_of_all_chars[s[r] mod 95].val %c s[r] %c\n", 
+                set_of_all_chars[s[r] % 95].val, s[r]);
+
+        printf(" set_of_all_chars[s[r] mod 95].index %d\n", 
+                set_of_all_chars[s[r] % 95].index);
+
+        if (set_of_all_chars[s[r] % 95].val == s[r]) {
+            // update l, remove from set
+            old_l = l;
+            l = set_of_all_chars[s[r] % 95].index + 1;
+            set_of_all_chars[s[r] % 95].index = r;
+            for (int i = 0; i < 95; i++) {
+                if ((set_of_all_chars[i].index < l - 1)
+                    && (set_of_all_chars[i].index >= old_l)) {
+                    set_of_all_chars[i].val = '\0';
+                    set_of_all_chars[i].index = -1;
+                }
+            }
+        }
+        else {
+            // add to set
+            set_of_all_chars[s[r] % 95].val = s[r];
+            set_of_all_chars[s[r] % 95].index = r;
+        }
+
+        cur_l = r - l + 1;
+
+        if (cur_l > max) {
+            max = cur_l;
+        }
+
+    }
+
+    return max;
+
+}
+
 int main() {
-    char *s = "abcde";
+    char *s = "abeeceeeee";
     printf("l %d\n", lengthOfLongestSubstring(s));
     return 0;
 }
