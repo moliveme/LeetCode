a9ad2bf7f86065a91abff7d26644fc413d9a7cf6
Mridu <mprasha@purdue.edu>
2023-06-06 17:56:54 +0530

    8-valid-parentheses

---
+++ 8-valid-parentheses/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean isValid(String s) {
+        
+        Stack<Character> STACK = new Stack<Character>();
+
+        int n = s.length();
+        char[] s_arr = s.toCharArray();
+        char popped;
+
+        for (int i = 0; i < n; i++) {
+            if ((s_arr[i] == '(')
+                || (s_arr[i] == '[')
+                || (s_arr[i] == '{')) { // if open, push
+
+                STACK.push(s_arr[i]);
+
+            } else { // if closed, pop and compare
+
+                popped = STACK.pop();
+                if ((s_arr[i] - popped != 1) &&
+                    (s_arr[i] - popped != 2)) {
+
+                    return false;
+
+                }
+
+            }
+        }
+        return STACK.empty();
+    }
+}
