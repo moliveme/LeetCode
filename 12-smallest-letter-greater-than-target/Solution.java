f34bdcb7396d588a18589959a4b137ef5d93337b
Mridu <mprasha@purdue.edu>
2023-06-09 21:19:11 +0530

    12-O(n)

--- 12-smallest-letter-greater-than-target/Solution.java
+++ 12-smallest-letter-greater-than-target/Solution.java
@@ -1,7 +1,17 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
 
-        char ret = 'a';
+        char ret = '|';
+
+        for (char ch : letters) {
+            if ((ch > target) && (ch < ret)) {
+                ret = ch;
+            }
+        }
+
+        if (ret == '|') {
+            ret = letters[0];
+        }
 
         return ret;
         
